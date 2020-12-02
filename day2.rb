8cebefb11a634356fa89980a96208124a95feb04
Damy van Valenberg <damyvv@hotmail.com>
2020-12-02 10:03:02 +0100

    Simplified parsing and solution for part 1

--- day2.rb
+++ day2.rb
@@ -1,19 +1,13 @@
 input = File.read("day2_input.txt").lines.map do |l|
-    a = l.split(' ')
-    mm = a[0].split('-')
-    { first: mm[0].to_i, second: mm[1].to_i, char: a[1][0], password: a[2] }
+    m = l.match(/(\d+)-(\d+) (\D): (\D+)/)
+    { first: m[1].to_i, second: m[2].to_i, char: m[3], password: m[4] }
 end
 
 # Part 1
-l = input.select do |i|
-    count = i[:password].split('').tally[i[:char]] || 0
-    count.between?(i[:first], i[:second])
-end
-puts l.size
+puts input.select {|i| i[:password].chars.count(i[:char]).between?(i[:first], i[:second]) }.size
 
 # Part 2
-l = input.select do |i|
-    chars = i[:password].split('')
+puts (input.select do |i|
+    chars = i[:password].chars
     (chars[i[:first]-1] == i[:char]) ^ (chars[i[:second]-1] == i[:char])
-end
-puts l.size
+end.size)
