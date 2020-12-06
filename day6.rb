68a67408b9e0a5751653aace327a2af6ffcb18ad
Damy van Valenberg <damyvv@hotmail.com>
2020-12-06 11:07:39 +0100

    Added another solution using sets

--- day6.rb
+++ day6.rb
@@ -1,3 +1,4 @@
+# Original solution
 input = File.read('day6_input.txt').strip.split("\n\n").map {|l| l.chars.tally }
 
 # Part 1
@@ -5,3 +6,15 @@ p input.map {|l| (l.keys - ["\n"]).count }.sum
 
 # Part 2
 p input.map {|l| l.filter {|k,v| v == ((l["\n"] || 0) + 1) }.keys.count }.sum
+
+
+# Another solution using sets
+require 'set'
+
+input = File.read('day6_input.txt').strip.split("\n\n").map {|l| l.split("\n").map {|s| s.chars.to_set } }
+
+# Part 1
+p input.map {|sl| sl.reduce(:|).count }.sum
+
+# Part 2
+p input.map {|sl| sl.reduce(:&).count }.sum
