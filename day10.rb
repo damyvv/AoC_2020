0ed7b567b30c99fc5416e968dca9440425898c89
Damy van Valenberg <damyvv@hotmail.com>
2020-12-10 12:53:56 +0100

    Added solution day 10

---
+++ day10.rb
@@ -0,0 +1,25 @@
+input = File.read("day10_input.txt").split.map(&:to_i)
+input += [0, input.max + 3]
+input.sort!
+
+# Part 1
+diff = input.each_cons(2).map {|a| a.reverse.reduce(:-) }
+p diff.tally.to_a.map {|e| e[1]}.reduce(:*)
+
+# Part 2
+def tribonacci(idx)
+    begin
+        @cache[0]
+    rescue
+        @cache = [0, 1, 1]
+    end
+    if @cache[idx]
+        @cache[idx]
+    else
+        @cache[idx] = (1..3).map {|n| tribonacci(idx-n) }.sum
+    end
+end
+
+groups = diff.join.split('3').select { |s| s.length > 0 }
+p groups.map {|s| tribonacci(s.length+1) }.reduce(:*)
+
