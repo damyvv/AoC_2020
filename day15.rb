cc9b9fe158c3695e7c8ac8a3c8a6c01c9039c61a
Damy van Valenberg <damyvv@hotmail.com>
2020-12-15 11:27:32 +0100

    Optimizations

--- day15.rb
+++ day15.rb
@@ -1,20 +1,15 @@
 input = File.read("day15_input.txt").strip.split(',').map(&:to_i)
 
-# Part 1
+# Part 1 & 2
 seen = input.map.with_index { |i,idx| { i => [idx+1] } }.reduce(:merge!)
 last = input[-1]
 turn = input.count
 
 while turn < 30000000
     turn += 1
-    if seen[last].count == 1
-        last = 0
-    else
-        last = seen[last][0] - seen[last][1]
-    end
-
+    last = seen[last][1] ? seen[last][0] - seen[last][1] : 0
     seen[last] = [] if !seen.key?(last)
-    seen[last].unshift(turn)
+    seen[last][0],seen[last][1] = turn,seen[last][0]
     p last if turn == 2020
 end
 p last
